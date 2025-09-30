177c9089e21cc2d73dc999a4e910a4ae5765c085
pangolecimal <pangolecimal@gmail.com>
2025-10-01 02:22:34 +0300

    runs on shimdows

--- nob.c
+++ nob.c
@@ -1,12 +1,15 @@
 #define NOB_IMPLEMENTATION
 #define NOB_STRIP_PREFIX
+#ifndef _WIN32
 #define NOB_EXPERIMENTAL_DELETE_OLD
+#endif
 #include "nob.h"
 
 int main(int argc, char **argv) {
     NOB_GO_REBUILD_URSELF(argc, argv);
     Cmd cmd = {0};
 
+#ifndef _WIN32
     cmd_append(&cmd, "test", "-d", ".build");
     if (!cmd_run(&cmd)) {
         cmd_append(&cmd, "mkdir", ".build");
@@ -20,14 +23,18 @@ int main(int argc, char **argv) {
     }
 
     nob_cc(&cmd);
-    cmd_append(&cmd, "-Wall", "-Wextra", "-pedantic", "-ggdb");
+    cmd_append(&cmd, "-Wall", "-Wextra", "-ggdb");
     cmd_append(&cmd, "-Wno-unused-parameter", "-Wno-unused-variable");
     cmd_append(&cmd, "-lX11");
     cmd_append(&cmd, "-o", ".build/exe", "src/main.c");
     if (!cmd_run(&cmd)) return 1;
-
-    cmd_append(&cmd, "./.build/exe");
+#else
+    cmd_append(&cmd, "cl", "/Fe:.build/windows", "/nologo", "/W3", "src/main.c", "/link", "User32.lib", "Gdi32.lib", "/subsystem:windows");
     if (!cmd_run(&cmd)) return 1;
+
+    cmd_append(&cmd, "./.build/windows.exe");
+    if (!cmd_run(&cmd)) return 1;
+#endif
 
     return 0;
 }
--- src/main.c
+++ src/main.c
@@ -2,7 +2,20 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <time.h>
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#else
 #include <X11/Xlib.h>
+#endif // _WIN32
+
+#ifdef _WIN32
+HBITMAP hbmp;
+HANDLE hTickThread;
+HWND hwnd;
+HDC hdcMem;
+#endif
 
 #define WIDTH  800
 #define HEIGHT 600
@@ -10,6 +23,11 @@
 typedef struct {
     uint8_t b, g, r, a;
 } Color;
+
+Color pixels[HEIGHT][WIDTH];
+
+#ifndef _WIN32
+// {{{
 
 int main(void) {
     Color pixels[WIDTH*HEIGHT] = {0};
@@ -133,3 +151,134 @@ int main(void) {
 
     return 0;
 }
+
+// }}}
+#else
+// {{{
+
+// https://www.daniweb.com/programming/software-development/code/241875/fast-animation-with-the-windows-gdi
+
+DWORD WINAPI tickThreadProc(HANDLE handle) {
+    Sleep(50);
+    ShowWindow(hwnd, SW_SHOW);
+    ShowCursor(FALSE);
+
+    HDC hdc = GetDC(hwnd);
+
+    hdcMem = CreateCompatibleDC(hdc);
+    HBITMAP hbmOld = (HBITMAP)SelectObject(hdcMem, hbmp);
+
+    // V2f ball1 = v2ff(400.0f);
+    // V2f ball2 = v2ff(0.0f);
+
+    for (;;) {
+        for (int y = 0; y < HEIGHT; y += 1) {
+            for (int x = 0; x < WIDTH; x += 1) {
+                pixels[y][x].r = 0xFF;
+                pixels[y][x].g = 0x00;
+                pixels[y][x].b = 0x00;
+                pixels[y][x].a = 0xFF;
+            }
+        }
+        BitBlt(hdc, 0, 0, WIDTH, HEIGHT, hdcMem, 0, 0, SRCCOPY);
+        break;
+    }
+
+    SelectObject(hdcMem, hbmOld);
+    DeleteDC(hdc);
+}
+
+void MakeSurface(HWND hwnd) {
+    BITMAPINFO bmi;
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFO);
+    bmi.bmiHeader.biWidth = WIDTH;
+    bmi.bmiHeader.biHeight = -HEIGHT;
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biBitCount = 32;
+    bmi.bmiHeader.biCompression = BI_RGB;
+    bmi.bmiHeader.biSizeImage = 0;
+    bmi.bmiHeader.biXPelsPerMeter = 0;
+    bmi.bmiHeader.biYPelsPerMeter = 0;
+    bmi.bmiHeader.biClrUsed = 0;
+    bmi.bmiHeader.biClrImportant = 0;
+    bmi.bmiColors[0].rgbBlue = 0;
+    bmi.bmiColors[0].rgbGreen = 0;
+    bmi.bmiColors[0].rgbRed = 0;
+    bmi.bmiColors[0].rgbReserved = 0;
+
+    HDC hdc = GetDC(hwnd);
+
+    hbmp = CreateDIBSection(hdc, &bmi, DIB_RGB_COLORS, (void **)&pixels, NULL, 0);
+    DeleteDC(hdc);
+
+    hTickThread = CreateThread(NULL, 0, &tickThreadProc, NULL, 0, NULL);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+    case WM_CREATE: {
+        MakeSurface(hwnd);
+    }
+    break;
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        BitBlt(hdc, 0, 0, WIDTH, HEIGHT, hdcMem, 0, 0, SRCCOPY);
+        EndPaint(hwnd, &ps);
+    }
+    break;
+    case WM_CLOSE: {
+        DestroyWindow(hwnd);
+    }
+    break;
+    case WM_DESTROY: {
+        TerminateThread(hTickThread, 0);
+        PostQuitMessage(0);
+    }
+    break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+    WNDCLASSEX wc;
+    MSG msg;
+
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.hbrBackground = CreateSolidBrush(0);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hInstance = hInstance;
+    wc.lpfnWndProc = WndProc;
+    wc.lpszClassName = "animation_class";
+    wc.lpszMenuName = NULL;
+    wc.style = 0;
+
+    if (!RegisterClassEx(&wc)) {
+        MessageBox(NULL, "Failed to register window class.", "Error", MB_OK);
+        return 0;
+    }
+
+    hwnd = CreateWindowEx(
+               WS_EX_APPWINDOW,
+               "animation_class",
+               "metaballs",
+               WS_MINIMIZEBOX | WS_SYSMENU | WS_POPUP | WS_CAPTION,
+               CW_USEDEFAULT, CW_USEDEFAULT, WIDTH, HEIGHT,
+               NULL, NULL, hInstance, NULL);
+
+    while (GetMessage(&msg, 0, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+// }}}
+#endif
